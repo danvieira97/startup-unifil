d5bd2cb3f9fc5a1a4405afa7a32df8ca194120a5
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-09-25 21:56:02 -0300

    adicionado docker na aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,8 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://daaaands:K5MvuE0JRNYhJuIL@cluster0.mzelhe5.mongodb.net/startup-unifil?retryWrites=true&w=majority',
+      // 'mongodb+srv://daaaands:K5MvuE0JRNYhJuIL@cluster0.mzelhe5.mongodb.net/startup-unifil?retryWrites=true&w=majority',
+      'mongodb://daaaands:K5MvuE0JRNYhJuIL@db:27017/startup-unifil?authSource=admin',
     ),
     ClientsModule,
     RestaurantsModule,
